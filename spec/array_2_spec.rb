dc55d816d25f5596eef6bbdf681e3ef05a16c1a7
Cara Bell <cara.bell@192.168.1.101>
2021-04-23 12:16:12 +0100

    explicit subject practice

---
+++ spec/array_2_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe Array do
+  subject(:sally) do
+    [1,2]
+  end
+
+  it 'has a length of 2' do
+    expect(subject.length).to eq(2)
+    subject.pop(1)
+    expect(subject.length).to eq(1)
+  end
+
+  it 'checks sally has worked as an alias' do
+    expect(subject).to eq(sally)
+  end
+end
